566e24a420d5891d74fc1b47c68a7dbb62070c6c
Milind_77 <milinddhote77@gmail.com>
2021-06-01 06:05:02 +0530

    [Milind] Add: Snake/ladder with single player at 0 position

---
+++ SnakeLadder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programming
+{
+	public class SnakeLadder
+	{
+		public static void Main(String[] args)
+		{
+			int playerPosition = 0;
+			Console.WriteLine("<-------Welcome to the Game of SNAKE & LADDER------->");
+			Console.WriteLine("Starting Position of the Player = " + playerPosition);
+		}
+	}
+}
