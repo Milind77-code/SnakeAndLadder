25bc33f0e5277a4b753222c4f35b3eb485606def
Milind Dhote <83074985+Milind77-code@users.noreply.github.com>
2021-07-07 00:10:30 +0530

    [Milind] Add: Player Rolls a die to get a number between (1-6)

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -12,9 +12,11 @@ namespace Programming
 			Console.WriteLine("<-------Welcome to the Game of SNAKE & LADDER------->");
            		Console.WriteLine("-----------------------------------------------------");
 			Console.WriteLine("Starting Position of the Player = " + playerPosition);
+			
 			//generating Random numbers from 1 to 6
 			Random random = new Random();
 		    	diceRoll = random.Next(1, 7);
+			
 			//Displays Dice Number
             		Console.WriteLine("Dice Number =" + " " + diceRoll);
 		}
